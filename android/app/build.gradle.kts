4aedbcc105ac9ce22461c6333916151ddc4bc920
Yash <yash-l@users.noreply.github.com>
2026-09-14 21:16:52 +0530

    Update HYDRA Android build project

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins { id("com.android.application") }
 
 android { namespace = "com.hydra.apex"; compileSdk = 35
-    defaultConfig { applicationId = "com.hydra.apex"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0.0" }
-    buildTypes { release { isMinifyEnabled = false; proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro") } }
+    defaultConfig { applicationId = "com.hydra.apex"; minSdk = 26; targetSdk = 35; versionCode = 100; versionName = "1.0.0" }
 }
